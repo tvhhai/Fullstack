f56893a60dfc710bc39889d5afbbb6075dc134b9
tvhhai <tranvanhoanghai2510@gmail.com>
2023-11-05 08:31:53 +0700

    refactor

--- todo/angular-client/src/app/app.component.spec.ts
+++ todo/angular-client/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
-import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    declarations: [AppComponent],
+    imports: [RouterTestingModule]
   }));
 
   it('should create the app', () => {
